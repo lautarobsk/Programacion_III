867865347708ec71559a48b7af13bbc0469179a4
lautaro arce <lautaroarce345@gmail.com>
2025-10-12 13:04:45 -0300

    Tp_6 Resuelto

---
+++ Tp1/Proyectos/gestor_estudiantes/apps/estudiante/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    cantidad_horas = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    edad = models.PositiveIntegerField()
+    nota_curso = models.DecimalField(max_digits=4, decimal_places=2)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
